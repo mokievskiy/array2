2e228a818da64d3e464a9f4a5018e35fc9f7e1ef
mokievskiy <44529729+mokievskiy@users.noreply.github.com>
2018-11-23 14:20:02 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int arr[] = {-5, -1, 0, 1};
+int arr[] = {5, -1, 0, 1};
+int arrs[] = {-1, 0, 1, 5};
 size_t n = sizeof (arr) / sizeof(int);
 
 void test1() {
@@ -62,7 +63,7 @@ void test2() {
 }
 
 void test3() {
-    cout << "Test #3: Testing pop_back and push_back operators\n";
+    cout << "Test #3: Testing += and -= operators\n";
     
     List<int> S(arr, n);
     List<int> R(arr, n);
@@ -74,19 +75,18 @@ void test3() {
     for (size_t i = 0; i < n - 1; i++) {
         b[i] = S[i];
     }
-    S.pop_back();
+    S-=;
     
     for (size_t i = 0; i < n; i++) {
         a[i] = S[i];   
     }
     a[n] = 3;
-    R.push_back(3);
+    R += 3;
     
     
     int ch = 0;
     for (size_t i = 0; i < n - 1; i++) {
         if (S[i] != b[i] || S.lenght() != n - 1) {
-            throw -2;
             ch = 1;
             cout << "Not correct\n";
             break;
@@ -94,7 +94,6 @@ void test3() {
     }
     for (size_t i = 0; i < n + 1; i++) {
         if (R[i] != a[i] || R.lenght() != n + 1) {
-            throw -2;
             ch = 1;
             cout << "Not correct\n";
             break;
@@ -109,5 +108,87 @@ void test3() {
 }
 
 void test4() {
-    cout << "Test #4: Testing 
+    cout << "Test #4: Testing print\n";
+    
+    List<int> S(arr,n);
+    
+    cout << S;
+    
+    return;
+}
+
+void test5() {
+    cout << "Test #5: Testing length function\n";
+    
+    List<int> S(arr, n);
+    size_t k;
+    k = S.length();
+    
+    int ch = 0;
+    if (k != n) {
+        ch = 1;
+        cout << "Test incorrect\n";
+    }
+    if (ch != 1) 
+        cout << "Test correct\n";
+    
+    return;
+}
+
+void test6() {
+    cout << "Test #6: Testing clear function\n";
+    
+    List<int> S(arr, n);
+    S.Clear;
+    
+    ch = 0;
+    
+    if (S[0] != 0) {
+        ch = 1;
+        cout << "Test incorrect\n";
+    }
+    
+    if (ch != 1)
+        cout << "Test correct\n";
+    
+    return;
+}
+
+void test7() {
+    cout << "Test #7: Testing [] operator\n";
+    
+    List<int> S(arr,n);
+    
+    int ch = 0;
+    for (size_t i = 0; i < n; i++) {
+        if (S[i] != arr[i]) {
+            ch = 1;
+            cout << "Not correct\n";
+            break;
+        }
+    }
+    if (ch != 1)
+        cout <<"Test correct\n";
+
+    return;
+}
+
+void test8() {
+    cout << "Test #8: Testing sorting\n";
+    
+    List<int> S(arr, n);
+    S.sorti();
+    
+    int ch = 0;
+    for (size_t i = 0; i < n; i++) {
+        if (S[i] != arrs[i]) {
+            ch = 1;
+            cout << "Not correct\n";
+            break;
+        }
+    }
+    if (ch != 1)
+        cout <<"Test correct\n";
+   
+    return;
 }
